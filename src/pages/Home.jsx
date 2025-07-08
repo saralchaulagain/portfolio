ab15b3670dbf1d759d81cfe683aae80c323077af
saralchaulagain <saral.chaulagain@gmail.com>
2025-07-08 06:02:25 +0545

    add meteors animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import { ThemeToggle } from "../components/ThemeToggle";
+import StarBackground from "../components/StarBackground";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <ThemeToggle />
+
+      {/* background  */}
+      <StarBackground />
     </div>
   );
 };
