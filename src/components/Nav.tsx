35f9aee68466b7f9b5ea27e35dbad17784f27cee
saralchaulagain <89011198+saralchaulagain@users.noreply.github.com>
2025-09-17 21:26:15 +0545

    bug fix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,46 @@
+"use client";
 import { Box, Button, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Nav = () => {
   const lists = ["About", "Skill", "Project", "Experience", "Education"];
+  const [dark, setDark] = useState(false);
+  useEffect(() => {
+    const mode = localStorage.getItem("mode");
+    if (mode === "true") {
+      setDark(true);
+    }
+  }, []);
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", dark);
+  }, [dark]);
   return (
-    <Box className="flex justify-between">
-      <Typography>Portfolio</Typography>
-      {lists.map((list, index) => (
-        <ul key={index} className="flex gap-2">
-          <a href="#">
-            <li>{list}</li>
-          </a>
-        </ul>
-      ))}
-      <Button>Contact</Button>
+    <Box className="flex justify-between items-center border-b-gray-1">
+      <Typography className="cursor-pointer" variant="h5">
+        Portfolio
+      </Typography>
+      <ul className="flex gap-6">
+        {lists.map((list, index) => (
+          <li key={index}>
+            <a href="#">{list}</a>
+          </li>
+        ))}
+      </ul>
+      <Box className="flex gap-2">
+        <Button variant="outlined" color="secondary">
+          Contact
+        </Button>
+        <Button
+          variant="outlined"
+          color="secondary"
+          onClick={() => {
+            setDark(!dark);
+            localStorage.setItem("mode", JSON.stringify(!dark));
+          }}
+        >
+          {dark ? "White?" : "Dark?"}
+        </Button>
+      </Box>
     </Box>
   );
 };
