9056214d2e0662efc87feaa8c1f801626c499802
saralchaulagain <89011198+saralchaulagain@users.noreply.github.com>
2025-09-19 18:38:47 +0545

    bug fix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -15,8 +15,12 @@ const Nav = () => {
     document.documentElement.classList.toggle("dark", dark);
   }, [dark]);
   return (
-    <Box className="flex justify-between items-center border-b-[0.5px] py-2">
-      <Typography className="cursor-pointer" variant="h5">
+    <Box className="w-full fixed top-0 right-0 left-0 flex justify-between items-center line px-2 py-3 bg-background/80 backdrop-blur-md z-50 shadow-sm">
+      <Typography
+        variant="h5"
+        className="cursor-pointer font-msMadi headers"
+        sx={{ fontFamily: "'Ms Madi', cursive" }}
+      >
         Portfolio
       </Typography>
       <ul className="flex gap-6">
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -5,12 +5,20 @@ import { FaInstagram, FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const About = () => {
   return (
-    <Box className="flex justify-center items-center mt-20">
+    <Box className="h-screen w-full flex justify-center items-center gap-3">
       <Box className="w-2/5 ">
-        <Typography variant="subtitle1">Hello, I&#39;m</Typography>
+        <Typography className="headers" variant="subtitle1">
+          Hello, I&#39;m
+        </Typography>
         <Box className="flex flex-col mt-6 gap-1">
-          <Typography variant="h3">Saral Chaulagain</Typography>
-          <Typography variant="h5">
+          <Typography
+            className="headers"
+            sx={{ fontFamily: "'Ms Madi', cursive" }}
+            variant="h3"
+          >
+            Saral Chaulagain
+          </Typography>
+          <Typography sx={{ fontFamily: "'Ms Madi', cursive" }} variant="h5">
             Full Stack Developer & UI/UX Designer
           </Typography>
         </Box>
@@ -26,10 +34,34 @@ const About = () => {
           <Button variant="outlined">Download CV</Button>
         </Box>
         <Box className="flex gap-1.5 text-xl mt-6">
-          <FaInstagram />
-          <FaFacebook />
-          <FaGithub />
-          <FaLinkedin />
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/saral.chaulagain12/?hl=en"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/saral.chaulagain.56/"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/saralchaulagain"
+          >
+            <FaGithub />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/saral-chaulagain-897986237/"
+          >
+            <FaLinkedin />
+          </a>
         </Box>
       </Box>
       <Box>
