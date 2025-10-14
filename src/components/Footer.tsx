5ba244fe1b7baa7ce253928ec13cd847b6920b62
saralchaulagain <89011198+saralchaulagain@users.noreply.github.com>
2025-10-14 21:54:13 +0545

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,12 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 const Footer = () => {
-  return <div>This is footer</div>;
+  return (
+    <Box className="my-6 ml-4">
+      © 2025 Saral Chaulagain. All rights reserved.
+    </Box>
+  );
 };
 
 export default Footer;
